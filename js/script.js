c5336307c9885afb32151017647dab31b8fa8503
Nikita Yershov <nikita.yershov@gmail.com>
2022-06-01 21:57:42 +0300

    Таймер обратного отсчета (нули вместо отрицательных значений)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//TABS START
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -11,49 +12,7 @@ function hideTabContent() {
 
     tabs.forEach(item => {
         item.classList.remove('tabheader__item_active');
-    }); 'use strict';
-
-const now = new Date();
-console.log(now); // 2022-06-01T09:31:13.487Z
-
-const userDate1 = new Date('2020-05-01');
-// new Date.parse('2020-05-01'); то же самое что метод выше
-console.log(userDate1); //2020-05-01T00:00:00.000Z
-
-// в формате аргументов месяца считаются с 0, а время считается с учетом часовых поясов
-const userDate2 = new Date(2020,5, 1, 20);
-console.log(userDate2);// 2020-06-01T17:00:00.000Z
-
-// любая дата в js хранится в милисекундах и отсчет ведется с 1.01.1970 года
-// (для боллее раннего переиода можно использовать отрицательные значения)
-
-// МЕТОДЫ GETTERs
-console.log(now.getFullYear()); // 2022
-console.log(now.getMonth()); // 5 (отсчет с 0; 6 => 5)
-console.log(now.getDate()); // 1 (отсчет с еденицы)
-console.log(now.getDay()); // день недели (воскресенье - 0, суббота - 6)
-console.log(now.getHours()); // 13 - местное время
-console.log(now.getUTCHours()); // 10 - время по UTC
-
-console.log(now.getTimezoneOffset()); // -180 Разница в минутах между настоящим и главным часовым поясом
-console.log(now.getTime());// количество милисекунд с  с 1.01.1970 (можно превратить в число поместив внутрь Data())
-
-// МЕТОДЫ SETTTERs
-
-const setDate = new Date();
-
-console.log(setDate.setHours(18));
-console.log(setDate)// 15:21:57 в консоли WS отображается UTC; в консоли браузера 18:24 – местное время
-
-//даты для измерения промежутков времени
-let start = new Date();
-
-for (let i = 0; i < 10000; i++) {
-    let some = i ** 3;
-}
-
-let end = new Date();
-console.log(`Цикл отработал за ${end - start} миллисекунд`);
+    }); 
 }
 
 function showTabContent(i = 0) {
@@ -76,3 +35,70 @@ tabsParent.addEventListener('click', (event) => {
         });
     }
 });
+// TABS END
+
+// TIMER START
+
+const deadline = '2022-05-02';
+
+function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+
+    const t = Date.parse(endtime) - Date.parse(new Date()); //превращаем строку в дату и отнимаем текущую дату
+
+    if (t <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+    days = Math.floor(t / (1000 * 60 * 60 * 24)); // количество дней до дедлайна
+    hours = Math.floor(t / (1000 * 60 * 60) % 24); // количество часов
+    minutes = Math.floor((t / 1000 / 60) % 60);
+    seconds = Math.floor((t / 1000) % 60);
+    }
+
+    return {
+        'total': t, //общее количество оставшихся милисекунд
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function getZero(num) { // добавляет ноль если значкение < 10
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+function  setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds');
+
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); // предотвращает мигание верстки(чтобы не ждать секунду после обновления страницы)
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.timer', deadline);
+
