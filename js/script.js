e28af48b774ee651b3c2b98fe972d65fed011c58
Nikita Yershov <nikita.yershov@gmail.com>
2022-06-26 13:58:27 +0300

    90. Получение данных с сервера. Async/Await (ES8)

--- js/script.js
+++ js/script.js
@@ -105,7 +105,6 @@ setClock('.timer', deadline);
 // MODAL START
 
 const modalShowBtns = document.querySelectorAll('[data-modal]'),
-      modalForm = document.querySelector('.modal form'),
       modal = document.querySelector('.modal');
 
 function openModal() {
@@ -149,9 +148,9 @@ window.addEventListener('scroll', showModalByScroll);
 
 // CARD TEMPLATES. CLASSES START
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-        this.src = src;
-        this.alt = alt;
+    constructor(img, altimg, title, descr, price, parentSelector, ...classes) {
+        this.img = img;
+        this.altimg = altimg;
         this.title = title;
         this.descr = descr;
         this.price = price; // price in USD
@@ -176,7 +175,7 @@ class MenuCard {
         }
 
         element.innerHTML = `
-                    <img src=${this.src} alt=${this.alt}>
+                    <img src=${this.img} alt=${this.altimg}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
                     <div class="menu__item-divider"></div>
@@ -189,36 +188,45 @@ class MenuCard {
     }
 }
 
-new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.' +
-    ' Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-).render();
+const getResource = async (url) => { // async ставится перед функцией
+    const res = await fetch(url);
 
-new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения,' +
-    ' молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и ' +
-    'импортных вегетарианских стейков.',
-    14,
-    ".menu .container"
-).render();
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.' +
-    ' Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    ".menu .container"
-).render();
+    return await res.json();
+};
+
+getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+        });
+    });
+
+// getResource('http://localhost:3000/menu')
+//     .then(data => createCard(data));
+
+// function createCard(data) {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//         const element = document.createElement('div');
+//
+//         element.classList.add("menu__item");
+//
+//         element.innerHTML = `
+//             <img src=${img} alt=${altimg}>
+//             <h3 class="menu__item-subtitle">${title}</h3>
+//             <div class="menu__item-descr">${descr}</div>
+//             <div class="menu__item-divider"></div>
+//             <div class="menu__item-price">
+//                 <div class="menu__item-cost">Цена:</div>
+//                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//             </div>
+//         `;
+//         document.querySelector(".menu .container").append(element);
+//     });
+// }
 
 // CARD TEMPLATES. CLASSES END
 
@@ -233,10 +241,23 @@ const message = {
 };
 
 forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
 });
 
-function postData(form) {
+const postData = async (url, data) => { // async ставится перед функцией
+    const res = await fetch(url, { // await ставится перед операциями, которые необходимо дождаться
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+    });
+
+    return await res.json();
+};
+
+
+function bindPostData(form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -248,35 +269,21 @@ function postData(form) {
         `;
         form.insertAdjacentElement('afterend', statusMessage); // позволяет расположить спиннер под формой
 
-
-
-
         const formData = new FormData(form); // в формах у input обязательно должен быть атрибут name !!!
 
-        const object = {}; // создаем объект ответа из формы
-        formData.forEach(function (value, key){
-            object[key] = value;
-        });
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-        const json = JSON.stringify(object); // преобразуем объект в json
-
-        fetch('server.php', {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: json
-        }).then(data => data.text())
+        postData('http://localhost:3000/requests', json)
             .then(data => {
-            console.log(data);
-            showThanksModal(message.success);
-            statusMessage.remove();
-        }).catch(() => {
-            showThanksModal(message.failure);
-        }).finally(() => {
-            form.reset(); // сбрасывает значения формы
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset(); // сбрасывает значения формы
+            });
         });
-    });
 }
 
 // FORMS END
@@ -306,3 +313,4 @@ function showThanksModal(message) {
 }
 
 // MODAL THANKS END
+
